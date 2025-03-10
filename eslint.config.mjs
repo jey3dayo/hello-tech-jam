75219582e669c464cc336e21d7a435e7049525e8
snakajim <snakajim@student.42tokyo.jp>
2025-03-10 18:27:55 +0900

    ADD: ra-men API

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // シングルクォートを使うとエラーにする
+      quotes: ["error", "double"],
+    },
+  },
 ];
 
 export default eslintConfig;
